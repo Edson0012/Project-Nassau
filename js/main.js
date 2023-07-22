33d266a8a94933f95bdce9f31bbaa201b353ea1a
Edson0012 <Edsonewertonrs1@gmail.com>
2023-07-22 13:27:53 -0300

    Completed Project Nassau, desktop and mobile

--- js/main.js
+++ js/main.js
@@ -2,7 +2,19 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
+        document.querySelector('.open-menu').src = "img/openicon.png";
     } else {
         menuMobile.classList.add('open');
+        document.querySelector('.open-menu').src = "img/closeicon.png";
+    }
+}
+
+function registeredEmail () {
+    const email = document.querySelector(".account").value;
+
+    if(email){
+        alert("Email registrado com sucesso!");
+    } else {
+        alert("Ocorreu um error, tente novamente mais tarde.")
     }
 }
